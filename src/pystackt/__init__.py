8bcfa3963e160bc8407cca864c713f424e50720d
Lien <lienbosmans@live.com>
2025-12-29 23:02:42 +0100

    Add get_ted_log to __init__ files

--- src/pystackt/__init__.py
+++ src/pystackt/__init__.py
@@ -1,4 +1,8 @@
-from pystackt.extractors import get_github_log
+from pystackt.extractors import (
+    get_github_log,
+    get_ted_log
+)
+
 from pystackt.exporters import (
     export_to_ocel2,
     export_to_promg
@@ -11,6 +15,7 @@ from pystackt.exploration import (
 
 __all__ = [  # Controls wildcard imports
     "get_github_log",
+    "get_ted_log",
     "export_to_ocel2",
     "export_to_promg",
     "create_statistics_views",
--- src/pystackt/extractors/__init__.py
+++ src/pystackt/extractors/__init__.py
@@ -1,3 +1,7 @@
 from pystackt.extractors.github import get_github_log
+from pystackt.extractors.ted import get_ted_log
 
-__all__ = ["get_github_log"]  # Controls wildcard imports
+__all__ = [
+    "get_github_log",
+    "get_ted_log"
+    ]  # Controls wildcard imports
