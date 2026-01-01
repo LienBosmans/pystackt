ac8a143f6531a3c62f0fcf9f6cbf1ab716d8115e
Lien <lienbosmans@live.com>
2026-01-01 22:39:17 +0100

    Move `_new_event_attributes()` and `_get_or_create_event_attribute()` to utils

--- src/pystackt/extractors/github/map_data.py
+++ src/pystackt/extractors/github/map_data.py
@@ -1,7 +1,10 @@
+import json
 from pystackt.utils.class_definitions import *
 from pystackt.extractors.github.get_data import _get_user_data
-from pystackt.utils.map_data import _get_or_create_event_type
-import json
+from pystackt.utils.map_data import (
+    _get_or_create_event_type,
+    _new_event_attributes
+)
 
 def _new_object_issue(issue_data:dict,object_types:dict,objects:dict,object_attributes:dict,object_attribute_values:dict) -> Object:
     '''Returns a new object of type `issue` and adds it to the objects dictionary.
@@ -261,44 +264,3 @@ def _new_timeline_event(issue_object,timeline_event_data:dict,event_types:dict,e
         return [new_event,user_data]
     else:
         return new_event
-
-
-def _new_event_attributes(new_event:Event,event_data:dict,event_attributes:dict,event_types:dict,event_attribute_values:dict) -> None:
-    """Sets the attributes of a new event and add them to the event_attribute_values dictionary."""
-
-    attributes = [['author_association','author_association'],]     # first string is key to get attribute, second string is key to use to extract from event_data
-
-    for attribute_def in attributes:
-        # get event_attribute with attribute_name
-        event_attribute = _get_or_create_event_attribute(attribute_def[0],new_event.event_type_description,event_types,event_attributes)
-
-        # extract attribute_value from issue_data with defined key
-        attribute_value = event_data.get(attribute_def[1])
-
-        if attribute_value is not None:
-            new_event_attribute_value = EventAttributeValue(new_event,event_attribute,attribute_value)
-            event_attribute_values[new_event_attribute_value.id] = new_event_attribute_value
-
-    return None
-
-
-def _get_or_create_event_attribute(attribute_description:str,event_type_description:str,event_types:dict,event_attributes:dict) -> EventAttribute:
-    """Use `attribute_description` and `event_type.description` as key to retrieve item from `event_attributes`.
-     If item does not exist, create new EventAttribute object and add it to `event_attributes`."""
-    
-    key = f"{event_type_description}:{attribute_description}"
-    event_attribute = event_attributes.get(key)
-
-    if event_attribute is None:
-        event_type = _get_or_create_event_type(event_type_description,event_types)
-
-        if attribute_description in ('author_association'):
-            datatype = 'varchar'
-        else: # catch-all just in case
-            print(f"Unknown attribute_description '{attribute_description}' detected, defaulted to 'string' as datatype. Please add this attribute in the function `get_or_create_event_attribute()` for next time.")
-            datatype = 'varchar'
-
-        event_attribute = EventAttribute(event_type,attribute_description,datatype)
-        event_attributes[key] = event_attribute
-
-    return event_attribute
--- src/pystackt/utils/map_data/__init__.py
+++ src/pystackt/utils/map_data/__init__.py
@@ -26,3 +26,43 @@ def _get_or_create_event_type(description:str,event_types:dict) -> EventType:
         event_types[description] = event_type
 
     return event_type
+
+def _new_event_attributes(new_event:Event,event_data:dict,event_attributes:dict,event_types:dict,event_attribute_values:dict) -> None:
+    """Sets the attributes of a new event and add them to the event_attribute_values dictionary."""
+
+    attributes = [['author_association','author_association'],]     # first string is key to get attribute, second string is key to use to extract from event_data
+
+    for attribute_def in attributes:
+        # get event_attribute with attribute_name
+        event_attribute = _get_or_create_event_attribute(attribute_def[0],new_event.event_type_description,event_types,event_attributes)
+
+        # extract attribute_value from issue_data with defined key
+        attribute_value = event_data.get(attribute_def[1])
+
+        if attribute_value is not None:
+            new_event_attribute_value = EventAttributeValue(new_event,event_attribute,attribute_value)
+            event_attribute_values[new_event_attribute_value.id] = new_event_attribute_value
+
+    return None
+
+
+def _get_or_create_event_attribute(attribute_description:str,event_type_description:str,event_types:dict,event_attributes:dict) -> EventAttribute:
+    """Use `attribute_description` and `event_type.description` as key to retrieve item from `event_attributes`.
+     If item does not exist, create new EventAttribute object and add it to `event_attributes`."""
+    
+    key = f"{event_type_description}:{attribute_description}"
+    event_attribute = event_attributes.get(key)
+
+    if event_attribute is None:
+        event_type = _get_or_create_event_type(event_type_description,event_types)
+
+        if attribute_description in ('author_association'):
+            datatype = 'varchar'
+        else: # catch-all just in case
+            print(f"Unknown attribute_description '{attribute_description}' detected, defaulted to 'string' as datatype. Please add this attribute in the function `get_or_create_event_attribute()` for next time.")
+            datatype = 'varchar'
+
+        event_attribute = EventAttribute(event_type,attribute_description,datatype)
+        event_attributes[key] = event_attribute
+
+    return event_attribute
